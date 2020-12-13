14b37cba1837ad70f4ea23da74cd7c281b6c2d75
Han-chanhui <malmizal486@naver.com>
2020-12-13 15:22:26 +0900

    added python file

---
+++ ex020.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+month = 48584 #한달에 내야 하는 비용 변수 지정
+total = 36 * month # * 연산자를 통해 한달 비용과 내야하는 달 수를 곱해 총 비용 계산
+print(total) #print() 함수를 통해 계산 결과 출력
+
