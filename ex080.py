beb4ea669514142ca0cf1cef355b576147293a48
Han-chanhui <malmizal486@naver.com>
2020-12-14 00:05:43 +0900

    added python file

---
+++ ex080.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+data = tuple(range(2,100,2))
+#range 함수는 범위가 정해진 연속된 숫자를 출력하는 함수이다.
+#range(범위 시작 숫자, 끝 숫자 - 1, 숫자의 간격) 을 의미한다. 이것을 tuple() 형태로 만든다.
+print(data) #print() 함수로 출력한다. 
+
