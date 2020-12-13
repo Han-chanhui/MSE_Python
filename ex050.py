1737292f565ce15e3179c519f5e7e92775dce8b4
Han-chanhui <malmizal486@naver.com>
2020-12-13 16:48:47 +0900

    added python file

---
+++ ex050.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+data = "039490    " #주어진 문자열(문자열의 오른쪽에 공백이 있다.)
+f = data.rstrip() # 멤버함수 rstrip()을 적용하여 오른쪽 공백을 삭제하고 그 결과값을 f로 지정한다. 
+print(f) #print() 함수를 이용해 결과값을 출력한다. 
+
+
+# In[ ]:
+
+
+
+
