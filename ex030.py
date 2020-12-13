03d335ab49616553e10595641838f43d1cb8fd4f
Han-chanhui <malmizal486@naver.com>
2020-12-13 15:23:54 +0900

    added python file

---
+++ ex030.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+string = 'abcd' #'문자열'의 형태로 변수 지정
+string.replace('b','B') 
+#replce 함수 사용 - 이때, replace는 리스트에 사용되는 멤버함수로, 문자열에는 적용되지 않는다.  
+print(string) #문자열을 출력한다. 
+
+
+# In[ ]:
+
+
+
+
