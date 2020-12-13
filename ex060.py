19828290094118e8440fd09b26c4ab203a94f90d
Han-chanhui <malmizal486@naver.com>
2020-12-13 22:54:43 +0900

    added python file

---
+++ ex060.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+nums = [1, 2, 3, 4, 5] #주어진 리스트
+average = sum(nums) / len(nums)
+#sum()은 리스트 내 원소의 값의 총 합을 구하는 함수이고 len()은 리스트 내의 원소의 개수를 구하는 함수이다.
+#따라서, 평균 = 원소의 총 합 / 원소의 개수로 나타낸다.
+print(average)
+
