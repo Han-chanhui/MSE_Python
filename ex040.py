1df16d4769eeaa04cf0824ceecbda2680c1965c5
Han-chanhui <malmizal486@naver.com>
2020-12-13 16:48:07 +0900

    added python file

---
+++ ex040.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+data = "   삼성전자   " #주어진 문자열(좌우공백이 있다.)
+f = data.strip() #파이썬의 멤버함수 strip()을 이용해 공백을 삭제하고 그 결과를 변수 f로 지정한다.
+print(f) #print() 함수를 이용해 결과를 출력한다. 
+
+
+# In[ ]:
+
+
+
+
