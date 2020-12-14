68738e5d77152368f020dd136edac8fe2f59778c
Han-chanhui <malmizal486@naver.com>
2020-12-14 20:31:51 +0900

    added python file

---
+++ ex250.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+import numpy as np #numpy를 np로 사용한다. 
+#arange를 사용하여 (시작, 끝, 간격)을 지정하고 시작과 끝 사이의 값을 일정한 간격의 배열로 출력한다. 
+for i in np.arange(0.0, 5.1, 0.1): 
+    print(i)
+
+
+# In[ ]:
+
+
+
+
